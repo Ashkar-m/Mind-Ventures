9836ec97d0754dc3426588367c771a00a266dca6
Ashkar-m <151915974+Ashkar-m@users.noreply.github.com>
2024-09-15 23:40:24 +0530

    work on serializers of login, register and otp

--- backend/users/views.py
+++ backend/users/views.py
@@ -8,13 +8,39 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from django.core.cache import cache
 from rest_framework.views import APIView
-from 
+from django.contrib.auth import authenticate
 
 import os
 import random
 
-from . serializers import OTPResendSerializer, RegisterSerializer, LoginSerializer
+from . serializers import OTPResendSerializer, RegisterSerializer
+from . serializers import ForgotPaswordSerializer, LoginSerializer, OTPVerificationSerializer
 # Create your views here.
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = authenticate(username=serializer.validate_data['username'], password=serializer.validate_data['password'])
+            if user is not None:
+                return Response({"message": "Login succefully"}, status=status.HTTP_200_OK)
+            else:
+                return Response({"message": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class OtpManger:
     def generate_otp():
@@ -54,6 +80,7 @@ def send_otp(request):
     
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 @api_view(['POST'])
 def verify_otp(request):
     serializer = OTPVerificationSerializer(data=request.data)
@@ -70,6 +97,7 @@ def verify_otp(request):
             return Response({'detail': 'Invalid OTP.'}, status=status.HTTP_400_BAD_REQUEST)
     
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['POST'])
 def resend_otp(request):
@@ -92,3 +120,15 @@ def resend_otp(request):
     
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class ForgotPasswordView(APIView):
+    def post(self, request):
+        serializer = ForgotPaswordSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validate_data['email']
+            if serializer.send_reset_email(email):
+                return Response({"message": "Password reset email sent succefully"}, status=status.HTTP_200_OK)
+            else:
+                return Response({"message": "Failed to sent the message"}, status=HTTP_500_INTERNAL_SERVER_ERROR)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
