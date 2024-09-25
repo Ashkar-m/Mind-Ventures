f8cea3919a2e776bc38fa43f2e825e2f6be12ca8
Ashkar-m <151915974+Ashkar-m@users.noreply.github.com>
2024-09-25 10:47:58 +0530

    updated mentor and admin dashboard

--- front-end/src/pages/Admin/Login/AdminLogin.jsx
+++ front-end/src/pages/Admin/Login/AdminLogin.jsx
@@ -92,11 +92,11 @@ return (
 
                 <label className="block pl-2 text-sm font-medium leading-6 text-gray-900" htmlFor="email">Email</label>
                 <input type="text" id="email" name='email'  
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                className="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
 
                 <label className="block pl-2 text-sm font-medium leading-6 text-gray-900" htmlFor="password">Password</label>
                 <input type="password" id="password" name='password' 
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                className="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
 
                 <button className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                  type="submit">Login</button>
